3e135bd59ecbedc72fd63cfe6999508ffb07d986
amador toledo <amadortoledo5@gmail.com>
2024-03-26 16:22:40 -0300

    lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListasComponent } from './listas/listas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/listas/listas.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listas',
+  templateUrl: './listas.component.html',
+  styleUrl: './listas.component.scss'
+})
+export class ListasComponent {
+  alumnosaprobados =["son goku","izuku midoriya","naruto uzumaki","bruce wayne"];
+  
+  alumnosdesaprobados =["sasuke uchiha","ichigo bakugo","shoto todoroki","clark kent"];
+ 
+}
